7ea3a9f14d42176cad129885e70d3fb48fd0224b
unknown <genismarbes@gmail.com>
2020-11-28 00:18:46 +0100

    Developed filtering consumptions by product category code, modified Consumption model to use UnixDateTimeField

--- consumptions/consumptions_api/models.py
+++ consumptions/consumptions_api/models.py
@@ -1,17 +1,24 @@
 from django.db import models
+from datetime import datetime
+from django_unixdatetimefield import UnixDateTimeField
 
 # Create your models here.
 class ProductCategories(models.Model):
     code = models.CharField(max_length=2, unique=True)
     name = models.CharField(max_length=10, null=False)
 
+    def __str__ (self):
+        return self.code
+
 class Products(models.Model):
     code = models.CharField(max_length=5, unique=True)
     name = models.CharField(max_length=25, null=False)
     category = models.ForeignKey(ProductCategories, to_field='code', on_delete=models.CASCADE)
 
+    def __str__ (self):
+        return self.code
+
 class Consumptions(models.Model):
-    timestamp = models.DateTimeField(auto_now_add=True)
+    timestamp = UnixDateTimeField(auto_now_add=True)
     product = models.ForeignKey(Products, to_field='code', on_delete=models.CASCADE)
     quantity = models.IntegerField(default=0)
-
--- consumptions/consumptions_api/views.py
+++ consumptions/consumptions_api/views.py
@@ -1,17 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-from rest_framework import generics
+from rest_framework import generics, filters
 from .serializers import ConsumptionSerializer
 from .models import Consumptions
 
 # Create your views here.
-def main(request):
-    return HttpResponse("Testing")
-
 class ConsumptionsViewList(generics.ListCreateAPIView):
-    queryset = Consumptions.objects.all()
     serializer_class = ConsumptionSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['product__category__code']
+
+    def get_queryset(self):
+        queryset = Consumptions.objects.all()
+        filterCategory = self.request.query_params.get('filterCategory', None)
+        if filterCategory is not None:
+            queryset = queryset.filter(product__category__code=filterCategory)
+        return queryset
 
 class ConsumptionsView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Consumptions.objects.all()
